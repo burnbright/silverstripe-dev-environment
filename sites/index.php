1117ceefebcb37633db9d4aef045d8a95cb56a2a
Jeremy Shipman <jeremy@burnbright.net>
2018-03-08 17:51:09 +1300

    Updated index page styling

--- sites/index.php
+++ sites/index.php
@@ -1,7 +1,30 @@
-<?php
-
-# Render a list of links to sub-folders
-$dirs = array_filter(glob('*'), 'is_dir');
-foreach ($dirs as $dir) {
-    echo "<a href='$dir'>$dir</a> <br>";
-}
+<html>
+    <head>
+        <title>sites</title>
+        <link rel="icon" type="image/png" href="favicon.png">
+        <style>
+            html{
+                font-family: sans-serif;
+                font-size: 18px;
+                padding: 50px;
+            }
+            a.website {
+                display: inline-block;
+                text-decoration: none;
+                margin-bottom: 1em;
+            }
+            a:hover {
+                text-decoration: underline;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+            # Render a list of links to sub-folders
+            $dirs = array_filter(glob('*'), 'is_dir');
+            foreach ($dirs as $dir) {
+                echo "<a class=\"website\" href='$dir'>$dir</a> <br>";
+            }
+        ?>
+    </body>
+</html>
